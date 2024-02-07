71460c02b5630c954ca85248620dabde9e0631e6
JustinBregu <justinb612@nycstudents.net>
2024-02-07 13:58:10 -0500

    Update Challenge 2.py

--- Challenge 2.py
+++ Challenge 2.py
@@ -1,20 +1,18 @@
-tip_percent=float(input())
 print('How was our service for you today')
-print('Bad(0%) , Okay(15%), Good(20%), Great(25%)')
-Service=(str(input()))
-if (Service =='Bad'):
-    tip_percent=0
-if (Service == 'Okay'):
+tip_percent = 0
+Service_a = input('Bad(0%) , Okay(15%), Good(20%), Great(25%)')
+Service = Service_a.lower()
+subtotal = float(input("What is your bill? "))
+if (Service =='bad'):
+    tip_percent = 0
+elif (Service == 'okay'):
     tip_percent=15
-if (Service == 'Good'):
-    tip_percent=20
-if (Service == 'Great'):
-    tip_percent=25
+elif (Service == 'good'):
+    tip_percent = 20
+elif (Service == 'great'):
+    tip_percent = 25
 
+tip = tip_percent*subtotal/100
+Total=tip+subtotal
+print(Total)
 
-subtotal=float(input())
-def Tip_calculator(tip_percent):
-    tip = tip_percent*subtotal/100
-    Total=tip+subtotal
-    print(Total)
-Tip_calculator
